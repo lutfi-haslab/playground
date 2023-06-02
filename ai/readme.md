79441ff35a1da95af9da4bf18e397d726bf0b38b
lutfi-haslab <work.lutfiikbalmajid@gmail.com>
2023-06-03 00:28:51 +0700

    update handle message

---
+++ ai/readme.md
@@ -0,0 +1,3 @@
+in linux or ubuntu server which not have display, you need to install bellow:
+sudo apt update 
+sudo apt install -y libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb libgbm-dev
--- ai/wabot.js
+++ ai/wabot.js
@@ -20,14 +20,17 @@ function start(client) {
   client.onMessage(async (message) => {
     try {
       const { body } = message;
-
-      const result = await sendMessage(body, message.chatId);
-      console.log(result);
-      console.log(message.chatId);
-
-      await client.sendText(message.from, result?.content);
+      if (body.startsWith("/ai ")) {
+        const arr = body.split(" ");
+        const words = arr.slice(1).join(" "); // "hello world this is lutfi"
+        // do something with words
+        const result = await sendMessage(words, message.chatId);
+        console.log(result);
+        console.log(message.chatId);
+        await client.sendText(message.from, result?.content);
+      }
     } catch (error) {
-      await client.sendText(
+      await console.log(
         message.from,
         "There is something wrong, or error in the systems. Please try again, or contact the admin"
       );
