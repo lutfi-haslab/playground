8c983596002f0d7f288e703dcad98bb68770f8cc
lutfi-haslab <work.lutfiikbalmajid@gmail.com>
2023-06-03 10:15:05 +0700

    add menu

--- ai/wabot.js
+++ ai/wabot.js
@@ -1,5 +1,6 @@
 import { create } from "@open-wa/wa-automate";
-import { sendMessage } from "./index.js";
+import { sendMessage, chatBot } from "./index.js";
+import { unsetListBot } from "./fn.js";
 
 // console.log(await sendMessage('Hello'))
 
@@ -20,9 +21,10 @@ function start(client) {
   client.onMessage(async (message) => {
     try {
       const { body } = message;
-      if (body.startsWith("/ai ")) {
-        const arr = body.split(" ");
-        const words = arr.slice(1).join(" "); // "hello world this is lutfi"
+      const arr = body.split(" ");
+      const words = arr.slice(1).join(" "); // "hello world this is lutfi"
+
+      if (body.startsWith("/chat ")) {
         // do something with words
         const { totalToken, msg } = await sendMessage(words, message.chatId);
         console.log(message.chatId);
@@ -30,8 +32,40 @@ function start(client) {
           message.from,
           msg?.content +
             "\n" +
-            `token used: ${totalToken}, cost $${((totalToken * 0.002) / 1000).toFixed(5)}`
+            `token used: ${totalToken}, cost $${(
+              (totalToken * 0.002) /
+              1000
+            ).toFixed(5)}`
         );
+      } else if (body.startsWith("/menu")) {
+        await client.sendText(
+          message.from,
+          "Halo, selamat datang di menu chatbot:\n" +
+            "/chat : untuk chatbot dengan context\n" +
+            "/reset : untuk reset context\n" +
+            "/ai : untuk tanya baru"
+        );
+      } else if (body.startsWith("/reset")) {
+        const reset = await unsetListBot(message.chatId);
+        if (reset) {
+          console.log("reset berhasil")
+          await client.sendText(message.from, "Reset berhasil");
+        }
+      } else if (body.startsWith("/ai ")) {
+        const { totalToken, msg } = await chatBot([
+          { role: "user", content: words },
+        ]);
+        await client.sendText(
+          message.from,
+          msg?.content +
+            "\n" +
+            `token used: ${totalToken}, cost $${(
+              (totalToken * 0.002) /
+              1000
+            ).toFixed(5)}`
+        );
+      } else {
+        undefined;
       }
     } catch (error) {
       await console.log(
